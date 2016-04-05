6ff46869ba6e4fe513570ff985fa66269f8afa29
Patryk <patryk.dom15@gmail.com>
2016-04-05 21:14:44 +0200

    Ready to start implementation API service

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        return $this->redirect('http://localhost/api/doc');
     }
 }
